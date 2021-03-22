8641f3c3c3bb854b746000a5d80b2d69974c4273
Kelvin Ng <kelvinng@interbase.com.my>
2021-03-22 18:09:45 +0800

    fix test

--- test/flutter_hms_gms_availability_test.dart
+++ test/flutter_hms_gms_availability_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_hms_gms_availability/flutter_hms_gms_availability.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('flutter_hms_gms_availability');
@@ -15,9 +14,5 @@ void main() {
 
   tearDown(() {
     channel.setMockMethodCallHandler(null);
-  });
-
-  test('getPlatformVersion', () async {
-    expect(await FlutterHmsGmsAvailability.platformVersion, '42');
   });
 }
