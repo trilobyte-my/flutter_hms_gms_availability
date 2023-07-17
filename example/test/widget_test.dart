56d861b738eb0a05de90fe8f6db474e8fd3406f0
Ivan Kurkin <ikurkin@tennisi.it>
2023-07-17 09:36:11 +0500

    update

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -1,26 +1,33 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
-import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:flutter_hms_gms_availability_example/main.dart';
 
 void main() {
-  testWidgets('Verify Platform version', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+  const MethodChannel channel = MethodChannel('flutter_hms_gms_availability');
 
-    // Verify that platform version is retrieved.
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  setUp(() {
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+        .setMockMethodCallHandler(channel, (MethodCall methodCall) async {
+      switch (methodCall.method) {
+        case 'isGmsAvailable':
+          return true;
+        case 'isHmsAvailable':
+          return false;
+        default:
+          throw MissingPluginException();
+      }
+    });
+  });
+
+  testWidgets('Verify Platform version', (WidgetTester tester) async {
+    await tester.pumpWidget(MyApp());
+    await tester.pumpAndSettle();
+
     expect(
-      find.byWidgetPredicate(
-        (Widget widget) => widget is Text &&
-                           widget.data.startsWith('Running on:'),
-      ),
+      find.text('GMS Available:  true\nHMS Available:  false'),
       findsOneWidget,
     );
   });
